be612524f1af6a770b84e07801b1772b6a80495a
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2023-12-13 22:43:52 +0100

    CMake, render: rename project to QwertzySynth

--- src/render.cpp
+++ src/render.cpp
@@ -2,7 +2,7 @@
 #include "app.hpp"
 
 Render::Render() {
-    window = SDL_CreateWindow("Music", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+    window = SDL_CreateWindow("QwertzySynth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE | SDL_RENDERER_PRESENTVSYNC);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
 }
